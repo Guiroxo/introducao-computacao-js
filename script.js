a997e4a013d362d23b6e3db882aacc46874372e2
Guiroxo <gui_rm_1998@hotmail.com>
2022-11-08 21:37:53 -0300

    resolução dos exercícios propostos

---
+++ script.js
@@ -0,0 +1,45 @@
+/* Exercício 1 */
+var age = 24;
+var name = "Guilherme";
+var men = true;
+var fruits = ["maça", "banana", "melão"];
+console.log(age);
+console.log(typeof age);
+console.log(name);
+console.log(typeof name);
+console.log(men);
+console.log(typeof men);
+console.log(fruits);
+console.log(typeof fruits);
+
+/* Exercício 5 */
+var number1 = 5;
+var number2 = 7;
+var number3 = 9;
+var media = (number1 + number2 + number3) / 3;
+console.log(media);
+
+/* Exercício 6 */
+var ap1 = 1.5;
+var ap2 = 2.3;
+var as = 5.2;
+var mediaFinal = ap1 + ap2 + as;
+console.log(mediaFinal);
+
+/* Exercício 7 */
+var nome = "Guilherme";
+var idade = 24;
+
+if (idade >= 18) {
+  console.log(`${nome} você é maior de idade`);
+} else {
+  console.log(`${nome} você é menor de idade`);
+}
+
+/* Exercício 8 */
+var n1 = 10;
+var n2 = 15;
+var n3 = 17;
+
+var max = Math.max(n1, n2, n3);
+console.log(max);
